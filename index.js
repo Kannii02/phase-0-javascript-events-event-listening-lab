9f2a48acfc0b12fa0146d650fc4dd09305848c54
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2024-12-22 16:48:54 -1000

    Add event listener implementation and pass tests for addingEventListener()

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('button'); // Select the element
+    input.addEventListener('click', function() {     // Attach event listener
+      alert('I was clicked!');
+    });
+  }
+  
