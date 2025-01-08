d96a4b6ad819846bba1c829ebf347a78d87a6020
kannikarsriburin <i_m_juy-juys@hotmail.co.th>
2025-01-07 21:06:07 -1000

    Fix test issues and finalize submission

--- test/helpers.js
+++ test/helpers.js
@@ -1,17 +1,21 @@
-import chai from 'chai';
-import { JSDOM } from 'jsdom';
+import { expect } from 'chai';
+global.expect = expect;
+
 import fs from 'fs';
 import path from 'path';
+import { JSDOM } from 'jsdom';
 
-global.expect = chai.expect;
-
+// Load HTML file
 const html = fs.readFileSync(path.resolve('index.html'), 'utf-8');
+
+// Create a DOM environment
 const dom = new JSDOM(html, { url: 'http://localhost' });
 
+// Set global variables to simulate browser environment
 global.window = dom.window;
 global.document = dom.window.document;
 
-// Mock localStorage to prevent errors
+// Mock localStorage if needed
 global.localStorage = {
   getItem: () => null,
   setItem: () => {},
